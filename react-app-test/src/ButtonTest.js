9cbb881f468fe2fe5edfd48561485eb6c65ba7b1
Jake Gustin <jgustin7b@gmail.com>
2023-03-31 08:54:17 -0400

    cheeky ButtonTest formatting update

--- react-app-test/src/ButtonTest.js
+++ react-app-test/src/ButtonTest.js
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import './ButtonTest.css';
 import { myName } from "./App.js";
 
 function ButtonTest() {
     return(
-        <div>
+        <div className="Button">
             <h1>Welcome to Button Test, {myName}!</h1>
+            <body>
+                <h2>Here are some of your most recent posts:</h2>
+            </body>
             <Link to="/">
                 <button type="button">
                     Return to Home
