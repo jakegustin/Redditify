3f21083489594de32e53e1350cce1b52774189bf
Jake Gustin <jgustin7b@gmail.com>
2023-04-03 18:20:06 -0400

    changing naming to use css file as the default in the future

--- frontend/redditify-webpage/src/ButtonTest.js
+++ frontend/redditify-webpage/src/ButtonTest.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import './ButtonTest.css';
+import './Default.css';
 import { myName } from "./App.js";
 
 function ButtonTest() {
     return(
-        <div className="Button">
+        <div className="Default">
             <h1>Welcome to Redditify, {myName}!</h1>
             <h2>Here are some of your most recent posts:</h2>
             <Link to="/">
